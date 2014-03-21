4e6f85b9cd03bea7f1bb33589f5df371e106412d
Zackary Ayoun <ayoun_z@epita.fr>
2014-03-21 04:23:28 +0100

    beat detection still in progress

--- include/detector.hh
+++ include/detector.hh
@@ -2,6 +2,10 @@
 # define DETECTOR_HH
 
 # include "sound-system.hh"
+# include <vector>
+
+# define WINDOW 4096
+# define RATIO 1.3
 
 class Detector
 {
@@ -11,9 +15,17 @@ class Detector
         ~Detector();
 
         SoundSystem& system_get();
+        int tempo_get();
+
+    private:
+        int nrj_get(int* data, int offset);
+        void peaks_set();
 
     private:
         SoundSystem system_;
+        std::vector<float> nrj1024_;
+        std::vector<float> nrj44100_;
+        std::vector<bool> peaks_;
 };
 
 #endif /* !DETECTOR_HH */
--- src/detector.cc
+++ src/detector.cc
@@ -6,6 +6,9 @@ Detector::Detector()
 
 Detector::Detector(SoundSystem& system)
     : system_(system)
+    , nrj1024_(system.len_get(), 0.f)
+    , nrj44100_(system.len_get(), 0.f)
+    , peaks_(system.len_get(), false)
 {
 }
 
@@ -16,4 +19,38 @@ Detector::~Detector()
 SoundSystem& Detector::system_get()
 {
     return system_;
+}
+
+int Detector::nrj_get(int* data, int offset)
+{
+    float ret = 0.f;
+    unsigned int max = offset + WINDOW;
+    for (unsigned int i = offset; (i < system_.len_get()) && (i < max); i++)
+        ret += ret + data[i] * data[i] / WINDOW;
+    return ret;
+}
+
+void Detector::peaks_set()
+{
+    for (unsigned int i = 0; i < system_.len_get() / 1024; i++)
+        nrj1024_[i] = nrj_get(system_.left_data_get(), 1024 * i);
+    float sum = 0.f;
+    for (unsigned int i = 0; i < 43; i++)
+        sum += nrj1024_[i];
+    nrj44100_[0] = sum / 43;
+    for (unsigned int i = 1; i < system_.len_get() / 1024; i++)
+    {
+        sum = sum - nrj1024_[i - 1] + nrj1024_[1 + 42];
+        nrj44100_[i] = sum / 43;
+    }
+    for (unsigned int i = 21; i < system_.len_get() / 1024; i++)
+        if (nrj1024_[i] > RATIO * nrj44100_[i - 21])
+            peaks_[i] = true;
+}
+
+int Detector::tempo_get()
+{
+    peaks_set();
+    //FIXME
+    return 0;
 }
