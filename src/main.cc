83f250733c83d2172bd5e27de839dba3b5fdbf05
Zackary Ayoun <ayoun_z@epita.fr>
2014-11-03 16:49:53 +0100

    Revert "BPM-Detector is now musical-feature-extractor"
    This reverts commit 83f16c03eea6aa11a0806cbb4957bd393cbcefd2.

--- src/main.cc
+++ src/main.cc
@@ -3,29 +3,21 @@
 
 int main(int argc, char** argv)
 {
-    if (argc == 2 && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")))
-        std::cout << "Help have to be done soon!" << std::endl;
-    else if (argc == 3)
+    if (argc != 2)
     {
-        if (!strcmp(argv[1], "-b") || !strcmp(argv[1], "--bpm"))
-        {
-            SoundSystem system;
-            system.load(argv[2]);
-            system.start();
-            system.pause();
-            Detector detect(system);
-            std::cout << detect.bpm_get() << std::endl;
-        }
-        else
-        {
-            std::cerr << argv[0] << ": use " << argv[0] << " --help" << std::endl;
-            return 1;
-        }
+        std::cerr << argv[0] << ": use " << argv[0] << " -h or --help." << std::endl;
+        return 1;
     }
+    else if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help"))
+        std::cout << argv[0] << ": usage: " << argv[0] << " <SONG FILE>." << std::endl;
     else
     {
-        std::cerr << argv[0] << ": use " << argv[0] << " --help" << std::endl;
-        return 1;
+        SoundSystem system;
+        system.load(argv[1]);
+        system.start();
+        system.pause();
+        Detector detect(system);
+        std::cout << detect.bpm_get() << std::endl;
     }
     return 0;
 }
